f6a1a8e4df64c3e349b5c7b7582fa286021a065b
tm2josep <tmammenjoseph@gmail.com>
2022-01-27 23:15:36 -0500

    clear to rewrite in go

--- interpreter/Symbol.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-@dataclass
-class Symbol:
-    name: str
-    def __repr__(self):
-        return f"Symbol({self.name})"
--- traits/Trait.py
+++
@@ -1,9 +0,0 @@
-from typing import Iterable
-from data_conatainers import FinEvent
-
-class Trait:
-    def __init__(self, name):
-        self.name = name
-    
-    def process(self, event_stack: Iterable[FinEvent]) -> Iterable[FinEvent]:
-        return event_stack
